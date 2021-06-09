c3d0194e1ba26f5584b9cec88dac34c915302806
Tokan Tair <tokan.tair@mail.ru>
2021-06-09 12:00:52 +0600

    kfc

---
+++ FRONT/data/nur-sultan-mix.js
@@ -0,0 +1,118 @@
+var myGeoJson ={
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "marker-color": "#7e7e7e",
+                "marker-size": "medium",
+                "marker-symbol": "",
+                "name": "Khan",
+                "zip code": 33700
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    71.4039659500122,
+                    51.13248110395433
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "stroke": "#555555",
+                "stroke-width": 2,
+                "stroke-opacity": 1,
+                "name": "line",
+                "zip code": 33700
+            },
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [
+                        71.40430927276611,
+                        51.1322656609499
+                    ],
+                    [
+                        71.40658378601074,
+                        51.13205021694033
+                    ],
+                    [
+                        71.40860080718994,
+                        51.137139813038665
+                    ],
+                    [
+                        71.41452312469482,
+                        51.13657433006457
+                    ],
+                    [
+                        71.4143943786621,
+                        51.1370051748635
+                    ],
+                    [
+                        71.41366481781006,
+                        51.13781299802523
+                    ]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "marker-color": "#7e7e7e",
+                "marker-size": "medium",
+                "marker-symbol": "",
+                "name ": "Sary",
+                "zip code ": 33700
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    71.41383647918701,
+                    51.1379476338447
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "stroke": "#555555",
+                "stroke-width": 2,
+                "stroke-opacity": 1,
+                "fill": "#555555",
+                "fill-opacity": 0.5,
+                "name ": "Ast",
+                "zip code": 33700
+            },
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [
+                            71.40954494476318,
+                            51.13458162057242
+                        ],
+                        [
+                            71.41143321990965,
+                            51.13458162057242
+                        ],
+                        [
+                            71.41143321990965,
+                            51.13536255751274
+                        ],
+                        [
+                            71.40954494476318,
+                            51.13536255751274
+                        ],
+                        [
+                            71.40954494476318,
+                            51.13458162057242
+                        ]
+                    ]
+                ]
+            }
+        }
+    ]
+}
+
---
+++ FRONT/data/plural.js
@@ -0,0 +1,329 @@
+var plural = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name":"Nur-Sultan"},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    71.43310546875,
+                    51.12421275782688
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    73.0810546875,
+                    49.809631563563094
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    76.9482421875,
+                    52.29504228453735
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    75.2783203125,
+                    51.713416052417614
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    71.87255859375,
+                    52.335339071889386
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    69.345703125,
+                    53.31774904749089
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    63.6328125,
+                    53.238920640924974
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    69.12597656249999,
+                    54.863962939854765
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    76.92626953125,
+                    43.26120612479979
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    71.4111328125,
+                    42.89206418807337
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    69.58740234375,
+                    42.32606244456202
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    78.37646484375,
+                    45.01141864227728
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    65.478515625,
+                    44.88701247981298
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    63.28125,
+                    45.61403741135093
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    61.63330078124999,
+                    46.78501604269254
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    57.19482421875,
+                    50.28933925329178
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    51.8994140625,
+                    47.12995075666307
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    51.15234375,
+                    43.6599240747891
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    51.39404296875,
+                    51.248163159055906
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    80.244140625,
+                    50.42951794712287
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    82.6171875,
+                    49.95121990866204
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    74.970703125,
+                    46.830133640447386
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    72.960205078125,
+                    50.05713877598689
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    70.1806640625,
+                    52.92215137976296
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    68.2635498046875,
+                    43.30119623257966
+                ]
+            }
+        }
+    ]
+}
+
+
+var poly1 = turf.polygon([[
+    [
+        71.34521484375,
+        51.134339258146774
+    ],
+    [
+        71.44752502441406,
+        51.134339258146774
+    ],
+    [
+        71.44752502441406,
+        51.20688339486559
+    ],
+    [
+        71.34521484375,
+        51.20688339486559
+    ],
+    [
+        71.34521484375,
+        51.134339258146774
+    ]
+]]);
+
+
+var poly2 = turf.polygon([[
+    [
+        71.39396667480469,
+        51.07764539352731
+    ],
+    [
+        71.53541564941405,
+        51.07764539352731
+    ],
+    [
+        71.53541564941405,
+        51.14877106901743
+    ],
+    [
+        71.39396667480469,
+        51.14877106901743
+    ],
+    [
+        71.39396667480469,
+        51.07764539352731
+    ]
+]]);
